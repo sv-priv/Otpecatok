02161919441529eb14acf00f865ce2491d16fcca
StefanVelkoski <velkoskis@gmail.com>
2020-11-04 14:09:14 +0100

    finalcommit2

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/jaglero2'));
 
-app.get('*', (req, res) => {
-    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+ '/dist/jaglero2/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
